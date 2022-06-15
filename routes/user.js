6ce67127e5cf31590816b431f7ba5c7bf7d2ec2c
Ashish K <ashishskabade@gmail.com>
2022-06-16 01:53:23 +0530

    group chat

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const data=require('./data');
+const express=require('express');
+const router=express.Router();
+router.get('/',(req,res,next)=>{
+    //console.log(1);
+    res.send('<form action="/" method="POST" ><input type="text" name="msg" onclick="document.getElementById(`username`).value=localStorage.getItem(`username`)"><input name="username" type="hidden" id="username"><button type="submit">send msg</button></form>');
+});
+router.post('/',(req,res,next)=>{
+    if(req.body.username!==undefined) data.push(`{ ${req.body.username} : ${req.body.msg}}`);
+    console.log(data);
+    //console.log(2);
+    res.redirect('/');
+})
+
+module.exports=router;
+
